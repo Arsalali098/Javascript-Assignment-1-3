5ecc1080a5a1560e5b7d38166d784b609017d034
Arsalali098 <135412892+Arsalali098@users.noreply.github.com>
2024-08-17 13:03:33 +0500

    Javascript Assignment 1-3

---
+++ Javasript Assignment/Assignment 1-3.js
@@ -0,0 +1,78 @@
+// Assignment # 1:: Alert Box 
+
+// Question 1
+alert("Hello Everbody!");
+
+//Question 2
+alert("Error! Please enter a valid password.");
+
+//Question 3
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Question 4
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// Question 5
+alert("Hello... I can run JS through my web browser's console");
+
+// Assignment # 2:: Variable for String
+
+// Question 1
+var username;
+
+// Question 2
+var myname = "Arsal Ali";
+
+// Question 3
+var message;
+message = "Hello World";
+alert(message);
+
+// Question 4
+var name = "Jhone Doe";
+var age = "15 year old";
+var pos = "Certified Mobile Application Development";
+
+alert(name);
+alert(age);
+alert(pos);
+
+// Question 5
+var abc = "PIZZA";
+var word = "";
+
+for(var i= abc.length; i>0; i--){
+    word += abc.slice(0,i) + "\n";
+}
+
+alert(word);
+
+// Question 6
+var email = "My eamil address is example@example.com";
+alert(email);
+
+// Question 7
+var book = "A smarter way to learn JavaScript";
+alert(book);
+
+// Assignment # 3:: Variable for Number
+
+// Question 1
+var age = "I am 19 years old";
+alert(age);
+
+// Question 2
+var N = 0;
+var visit = ++N;
+alert("You have visisted this site "+ visit +" times");
+
+// Question 3
+var birthyear = 2005;
+console.log("My birth year is "=birthyear+"\n Data type of my declared variable is number")
+
+// Question 4
+var visitor = "Arsal"
+var product = "T shirt";
+var quantity = 5;
+console.log(visitor+" ordered "+quantity+ product+" on XYZ clothing store");
